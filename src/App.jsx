b318b07081869dc504e5e466d18656d8ec1e7856
Leon Chapman <lchapman9955@gmail.com>
2025-11-19 19:47:09 +0000

    new file:   data/cities.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/bg.jpg 	new file:   public/icon.png 	new file:   public/img-1.jpg 	new file:   public/img-2.jpg 	new file:   public/logo.png 	modified:   src/App.jsx 	new file:   src/components/AppNav.jsx 	new file:   src/components/AppNav.module.css 	new file:   src/components/Button.module.css 	new file:   src/components/City.jsx 	new file:   src/components/City.module.css 	new file:   src/components/CityItem.module.css 	new file:   src/components/CityList.jsx 	new file:   src/components/CityList.module.css 	new file:   src/components/CountryItem.jsx 	new file:   src/components/CountryItem.module.css 	new file:   src/components/CountryList.module.css 	new file:   src/components/Footer.jsx 	new file:   src/components/Form.jsx 	new file:   src/components/Form.module.css 	new file:   src/components/Logo.jsx 	new file:   src/components/Logo.module.css 	new file:   src/components/Map.jsx 	new file:   src/components/Map.module.css 	new file:   src/components/Message.jsx 	new file:   src/components/Message.module.css 	new file:   src/components/PageNav.jsx 	new file:   src/components/PageNav.module.css 	new file:   src/components/Sidebar.jsx 	new file:   src/components/Sidebar.module.css 	new file:   src/components/Spinner.jsx 	new file:   src/components/Spinner.module.css 	new file:   src/components/SpinnerFullPage.jsx 	new file:   src/components/SpinnerFullPage.module.css 	new file:   src/components/User.jsx 	new file:   src/components/User.module.css 	new file:   src/data/cities.json 	new file:   src/index.css 	modified:   src/main.jsx 	new file:   src/pages/AppLayout.jsx 	new file:   src/pages/AppLayout.module.css 	new file:   src/pages/Homepage.jsx 	new file:   src/pages/Homepage.module.css 	new file:   src/pages/Login.jsx 	new file:   src/pages/Login.module.css 	new file:   src/pages/PageNotFound.jsx 	new file:   src/pages/Pricing.jsx 	new file:   src/pages/Product.jsx 	new file:   src/pages/Product.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+
 function App() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      {/* Can be sued to apply effects across all webpages which are linked below to it
+      <h1>hello router!</h1> */}
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="App" element={<AppLayout />}>
+            <Route index element={<CityList />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="countries" element={<p>List of countries</p>} />
+            <Route path="form" element={<p>Forms</p>} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
