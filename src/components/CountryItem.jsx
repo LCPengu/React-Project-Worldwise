bf35a0432698e44928a5ae14f63b49ee1ee139ad
LCPengu <LChapman9955@gmail.com>
2025-11-25 20:03:15 +0000

    modified:   src/components/CountryItem.jsx

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,15 +1,10 @@
-//import Button from "./Button";
 import styles from "./CountryItem.module.css";
-import { Link } from "react-router-dom";
 
 function CountryItem({ country }) {
-  console.log(country);
   return (
     <li className={styles.countryItem}>
-      <Link to={`${country.country}`} className={styles.cityItem}>
-        <span>{country.emoji}</span>
-        <span>{country.country}</span>
-      </Link>
+      <span>{country.emoji}</span>
+      <span>{country.country}</span>
     </li>
   );
 }
