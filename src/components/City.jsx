d26604ede9eeb9bfd118b0d2152f3378b58cfabd
Leon Chapman <lchapman9955@gmail.com>
2025-11-26 15:50:19 +0000

    modified:   data/cities.json 	modified:   src/components/City.jsx 	modified:   src/components/Map.jsx 	new file:   src/hooks/useGeolocation 	new file:   src/hooks/useUrlPosition.js

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useCities } from "../context/CitiesContext.jsx";
-//import BackButton from "./BackButton";
+import { useCities } from "../context/CitiesContext";
+import BackButton from "./BackButton";
 import styles from "./City.module.css";
 import Spinner from "./Spinner";
-import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,66 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import styles2 from "./button.module.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../context/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   const navigate = useNavigate();
-
-  const [searchParams, setSearchParams] = useSearchParams();
-
+  const { cities } = useCities();
+  const {
+    isLoading: isLoadingPosition,
+    position: geoloactionPosition,
+    getPosition,
+  } = useGeolocation();
+  const [lat, lng] = useUrlPosition();
+  /* 
   const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const lng = searchParams.get("lng"); */
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
+  useEffect(
+    function () {
+      if (lat && lng) {
+        setMapPosition([lat, lng]);
+      } else {
+        setMapPosition([40, 0]);
+      }
+    },
+    [lat, lng]
+  );
+
+  useEffect(
+    function () {
+      if (geoloactionPosition) {
+        setMapPosition([geoloactionPosition.lat, geoloactionPosition.lng]);
+      }
+    },
+    [geoloactionPosition]
+  );
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div
+      className={styles.mapContainer}
+      onClick={(e) => {
+        navigate("form");
+        //navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+      }}
+    >
+      {geoloactionPosition && (
+        <button className={styles2.position} onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={13}
@@ -24,14 +71,29 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker position={mapPosition} key={city.id}>
+            <Popup>{city.cityName}</Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
