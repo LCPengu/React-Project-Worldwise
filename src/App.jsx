e09edc0d529ebdbeda1ee29586645a2c9982e2ff
Leon Chapman <lchapman9955@gmail.com>
2025-11-28 15:02:45 +0000

    modified:   src/App.jsx 	renamed:    src/context/FathAuthConext.jsx -> src/context/FakeAuthConext.jsx 	modified:   src/pages/Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CountriesList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./context/CitiesContext.jsx";
+import { AuthProvider } from "./context/FakeAuthConext.jsx";
 
 function App() {
   return (
@@ -20,19 +21,21 @@ function App() {
 
       <BrowserRouter>
         <CitiesProvider>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="App" element={<AppLayout />}>
-              <Route index path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountriesList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <AuthProvider>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/app" element={<AppLayout />}>
+                <Route index path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountriesList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </AuthProvider>
         </CitiesProvider>
       </BrowserRouter>
     </div>
